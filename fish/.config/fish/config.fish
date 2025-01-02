dd8cc513b2f7b474317d5d178029c2816ff4af3e
Aaron Wu <demonaarwu@tuta.io>
2025-01-02 17:58:07 +0800

    2025-01-02

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,11 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    alias cd=z
     alias pyactivate=". venv/bin/activate.fish"
     alias em="emacs -nw"
     alias et="emacsclient -a \"\" -t"
-    zoxide init fish | source
     fzf --fish | source
+    source "$HOME/.cargo/env.fish"  # For fish
     set -x EDITOR nvim
     set -x TERMINAL alacritty
     set -U fish_user_paths ~/.cargo/bin $fish_user_paths
