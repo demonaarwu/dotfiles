4780b583521ef9ba02fcdf0091c175771577d7d6
Aaron Wu <demonaarwu@tuta.io>
2025-01-02 18:00:55 +0800

    Add Emacs

---
+++ emacs/.emacs.d/lisp/init-ivy.el
@@ -0,0 +1,18 @@
+;; init-ivy.el -- Configuration for Ivy
+
+(use-package amx
+  :ensure t
+  :init
+  (amx-mode 1))
+
+(use-package ivy
+    :ensure t
+    :init
+    (ivy-mode 1)
+    )
+
+(use-package counsel
+  :ensure t)
+
+(provide 'init-ivy)
+;; init-ivy.el ends here
