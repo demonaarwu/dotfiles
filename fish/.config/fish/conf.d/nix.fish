df5c46a39eedf7c31f097a580921e0dedfc49fd8
Aaron Wu <demonaarwu@tuta.io>
2025-06-24 21:05:16 +0800

    2025-06-24

---
+++ fish/.config/fish/conf.d/nix.fish
@@ -0,0 +1,2 @@
+
+if test -e /home/demonaarwu/.nix-profile/etc/profile.d/nix.fish; . /home/demonaarwu/.nix-profile/etc/profile.d/nix.fish; end # added by Nix installer
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,13 +6,20 @@ if status is-interactive
     zoxide init fish | source
     fzf --fish | source
     alias cd="z"
-    source "$HOME/.cargo/env.fish"  # For fish
-    set -x EDITOR nvim
+    set -x EDITOR emacs
     set -x TERMINAL alacritty
+    . /home/demonaarwu/.nix-profile/etc/profile.d/nix.fish
     set -U fish_user_paths ~/.cargo/bin $fish_user_paths
+    set -Ua fish_features no-keyboard-protocols
+
     set -U fish_user_paths ~/.local/bin $fish_user_paths
+    # source ~/.venv/bin/activate.fish
     function backup
     	git add .
         git commit -m (date +%Y-%m-%d)
     end
 end
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
